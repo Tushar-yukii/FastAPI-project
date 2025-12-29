ed11b8d49a69cf727c717f861c7cf9db3c9fdba7
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-12-29 23:26:10 +0530

    FastAPI

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from fastapi import FastAPI
 
-
 app = FastAPI() # instance 
-
 
 
 @app.get('/') # decorater
 def index(): # function
     return {'data': {'blog list'}}
 
-
-
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data':'all unpublished blog'}
-
 
 @app.get('/blog/{id}')
 def show(id: int):
     return {'data': id}
 
-
-
 @app.get('/blog/{id}/comments')
+# comments 
 def comments(id):
     return {'data': {'1', '2', '3'}}
 
