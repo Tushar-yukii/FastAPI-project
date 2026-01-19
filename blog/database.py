ac96c6886ef72e77e2cc72cac8a57d02a894a2a8
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-01-19 23:29:43 +0530

    Commit

--- blog/database.py
+++ blog/database.py
@@ -1,13 +1,32 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declaration import declaration_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///:/blog.db'
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL,{"check_same_thread": False})
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
 
-base = declaration_base()
-# error
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declaration import declaration_base
+# from sqlalchemy.orm import sessionmaker
+
+
+# SQLALCHAMY_DATABASE_URL = 'sqlite:///:/blog.db'
+
+# engine = create_engine(SQLALCHAMY_DATABASE_URL,{"check_same_thread": False})
+
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+# base = declaration_base()
