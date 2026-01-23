3f94a25413722407c9600380473d606f0aae4a7c
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-01-23 16:06:20 +0530

    FastAPI

--- blog/main.py
+++ blog/main.py
@@ -16,11 +16,18 @@ def get_db():
 modals.Base.metadata.create_all(engine)
 
 @app.post('/blog')
-def create(request:schemas.Blog, db : Session = Depends(get_db)):
+def create(request:schemas.Blog, db : Session = Depends(get_db)): # database instance
     new_blog = modals.Blog(title=request.title, body = request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def all(db : Session = Depends(get_db)):
+    blogs = db.query(modals.Blog).all()
+    return blogs
+
 
 
