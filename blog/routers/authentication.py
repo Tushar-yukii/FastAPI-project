370d023f5d8060c96c1ca412e91f5e7734fa3ea9
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-02-25 02:48:09 +0530

    FastAPI

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,21 +1,32 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, database, modals
 from sqlalchemy.orm import Session 
-
+from ..hashing import Hash
 router = APIRouter(
     tags=['Authentication']
 )
 # error
 @router.post('/login')
 def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+    
     user = db.query(modals.User).filter(
         modals.User.email == request.username
     ).first()
 
+    # PRINTS HERE
+    print("Username:", request.username)
+    print("User found:", user)
+
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials"
         )
-
+    if not Hash.verify(request.username, request.password):
+         raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="incorrect password"
+        )
+        
     return user
+
